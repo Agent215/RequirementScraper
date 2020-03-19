fa17ee8a8bc3e7c93ff5174507e2fd216f596227
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-03-19 11:37:54 -0400

    Python script now checks what OS its on and formats path accordingly

--- src/java/python/OpenPythonTest.java
+++ src/java/python/OpenPythonTest.java
@@ -12,32 +12,45 @@ grabs the 40th even table row HTML DOM element from the requirements page for CI
 package python;
 
 import java.io.*;
+import java.nio.file.Paths;
 import model.courses.StringDataList;
 
 /**
  *
- * This is a class to open and execute the python script to scrape
- * from Temples public facing web page for CIS course requirements For now the
- * path the python is specific to my machine. If you want this to work you
- * should change the Abspath variable to match your machine. When this is living
- * in a server we can use use the servers file system address and just leave it
- * as is.
+ * This is a class to open and execute the python script to scrape from Temples
+ * public facing web page for CIS course requirements For now the path the
+ * python is specific to my machine. If you want this to work you should change
+ * the Abspath variable to match your machine. When this is living in a server
+ * we can use use the servers file system address and just leave it as is.
  */
 public class OpenPythonTest {
 
+    private static String OS = System.getProperty("os.name").toLowerCase();
+
     /**
-     * @param args the command line arguments
      * @return
      */
-      public static StringDataList runScraper() {
+    public static StringDataList runScraper() {
+
         StringDataList list = new StringDataList();
         try {
 
             final int PATHMOD = 36; // number of chars to remove to build path
-          
-            // add a list to return the data as list of strings for
-            // easy parsing to json
-            // if this is on a server then we can use an absolute path 
+
+            // check os to format path with correct backslash
+            if (isWindows()) {
+                String relativePath = "\\testScraper.py";
+                System.out.println("This is running on Windows");
+            } else if (isMac()) {
+                String relativePath = "//testScraper.py";
+                System.out.println("This running on a Mac");
+            } else if (isUnix()) {
+                String relativePath = "//testScraper.py";
+                System.out.println("This is Unix or Linux");
+            } else {
+                System.out.println("This Os is not supported!!");
+            }
+
             String relativePath = "\\testScraper.py"; // this should always stay the same
             // get location of class in project on machine.
             final File f = new File(OpenPythonTest.class.getProtectionDomain().getCodeSource().getLocation().getPath());
@@ -75,5 +88,25 @@ public class OpenPythonTest {
         }
 
     } // end run Scraper
+
+    //some helper methods for checkin OS
+    //****************************************************************************
+    public static boolean isWindows() {
+
+        return (OS.indexOf("win") >= 0);
+
+    }
+
+    public static boolean isMac() {
+
+        return (OS.indexOf("mac") >= 0);
+
+    }
+
+    public static boolean isUnix() {
+
+        return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0);
+
+    }
 
 } // end OpenPythonTest.java
