774d6f73d80f3d78395e229578f5848307fd6e09
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-03-27 20:33:54 -0400

    fixed endpoint titles

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -12,32 +12,32 @@ def index():  # for now this justs runs the testing script
     return  jsonify(courses=DarsScrape())
 
 #add a user and password to databse
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/api/signup', methods=['GET', 'POST'])
 def signup():
     return "sign up content coming soon"
 
 #take user logged in and get all courses taken
-@app.route('/getCourses', methods=['GET', 'POST'])
+@app.route('/api/getCourses/<user_id>', methods=['GET', 'POST'])
 def getCourses():
-    return "getCourses content coming soon"
+       return  jsonify(courses=DarsScrape())
 
 #take user and get course requrirements
-@app.route('/getRequirements', methods=['GET', 'POST'])
+@app.route('/api/getRequirements', methods=['GET', 'POST'])
 def getRequirements():
       return "getRequirements content coming soon"
 
 #return all users in db
-@app.route('/getUsers')
+@app.route('/api/getUsers')
 def getUsers():
     return "getUsers content coming soon"
     
 #take users and scrape all courses
-@app.route('/scrapeCourses')
+@app.route('/api/scrapeCourses')
 def scrapeCourses():
     return  jsonify(courses=DarsScrape())
 
 #take user and scrape all courses requirements for them
-@app.route('/scrapeReqs')
+@app.route('/api/scrapeReqs')
 def scrapeReqs():
     return  "scrapeReqs content coming soon"
     
