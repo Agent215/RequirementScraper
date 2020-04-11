ff061d86e0108bbdb2a5ebe1f859ad43aa7d7df6
Evan Fiordeliso <evan.fiordeliso@temple.edu>
2020-04-11 12:12:40 -0400

    Redesign
    * Add themed screenshots
    * Changed look of courses and requirements pages

--- frontend/src/pages/dashboard/courses.js
+++ frontend/src/pages/dashboard/courses.js
@@ -1,15 +1,53 @@
 import React from "react";
 import {connect} from "react-redux";
 import {fetchCompleted} from "../../actions/courses";
-import {Alert, Card, Container, Spinner, Table} from "react-bootstrap";
-import ThemedCard from "../../components/card";
+import {Alert, Container, Jumbotron, Spinner, Table} from "react-bootstrap";
 
 class CourseItem extends React.Component {
+    formatGrade(grade) {
+        switch (grade) {
+            case "RG":
+                return "Registered";
+            case "CR":
+                return "Credit (Credit/No-Credit)";
+            case "CD":
+                return "Credit D+, D, or D- (Credit/No-Credit)";
+            case "NC":
+                return "No Credit (Credit/No-Credit)";
+            case "AU":
+                return "Audit";
+            case "I":
+                return "Incomplete";
+            case "IC":
+                return "Incomplete (Credit/No-Credit)";
+            case "IP":
+                return "Incomplete (Pass/Fail)";
+            case "M":
+                return "Military Leave of Absence";
+            case "MG":
+                return "Grade Missing";
+            case "NR":
+                return "Grade Not Reported";
+            case "P":
+                return "Passed";
+            case "PI":
+                return "Permanent Incomplete";
+            case "W":
+                return "Withdrawn";
+            case "WE":
+                return "Excused Withdrawal";
+            case "WS":
+                return "Withdrawn from Semester (Historical)"
+            default:
+                return grade;
+        }
+    }
+
     render() {
         return <tr>
             <td>{ this.props.course.CRN }</td>
             <td>{ this.props.course.Credit }</td>
-            <td>{ this.props.course.Grade }</td>
+            <td>{ this.formatGrade(this.props.course.Grade) }</td>
             <td>{ this.props.course.Name }</td>
             <td>{ this.props.course.Term }</td>
         </tr>
@@ -36,12 +74,9 @@ class Courses extends React.Component {
     render() {
         if (this.state.loading && this.props.courses.loaded) this.setState({ loading: false });
 
-        return <Container>
-            <ThemedCard className={[ "my-5", "p-5" ]}><Card.Body>
-                <h1 className="mb-5">Completed Courses</h1>
-                { this.props.courses.error !== null ? this.error() : (this.state.loading ? this.loading() : this.loaded()) }
-            </Card.Body></ThemedCard>
-        </Container>
+        return <div>
+            { this.props.courses.error !== null ? this.error() : (this.state.loading ? this.loading() : this.loaded()) }
+        </div>
     }
 
     loading() {
@@ -52,20 +87,32 @@ class Courses extends React.Component {
     }
 
     loaded() {
-        return <Table striped bordered hover variant={this.props.theme.dark ? "dark" : "secondary"}>
-            <thead>
-                <tr>
-                    <th>Course</th>
-                    <th>Credits</th>
-                    <th>Grade</th>
-                    <th>Name</th>
-                    <th>Term</th>
-                </tr>
-            </thead>
-            <tbody>
-                { Object.entries(this.props.courses.completed).map(([key, course]) => <CourseItem key={key} course={course} />) }
-            </tbody>
-        </Table>
+        const bg = this.props.theme.dark ? "bg-secondary" : "bg-gray";
+        return <>
+            <Jumbotron className={bg}>
+                <h1>Courses</h1>
+                <p>
+                    The following are all of the courses you have registered for, the amount of credits it counts for,
+                    the grade you received, and the term you completed the course.
+                </p>
+            </Jumbotron>
+            <Container fluid>
+                <Table striped bordered hover variant="secondary">
+                    <thead>
+                    <tr>
+                        <th>Course</th>
+                        <th>Credits</th>
+                        <th>Grade</th>
+                        <th>Name</th>
+                        <th>Term</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    { Object.entries(this.props.courses.completed).map(([key, course]) => <CourseItem key={key} course={course} />) }
+                    </tbody>
+                </Table>
+            </Container>
+        </>
     }
 
     error() {
--- frontend/src/pages/dashboard/requirements.js
+++ frontend/src/pages/dashboard/requirements.js
@@ -1,50 +1,84 @@
 import React from "react";
 import {connect} from "react-redux";
 import {fetchRequirements} from "../../actions/requirements";
-import {Alert, Spinner} from "react-bootstrap";
+import {Alert, ButtonGroup, Spinner, Jumbotron} from "react-bootstrap";
+import ThemedCard from "../../components/card";
+import ThemedButton from "../../components/button";
 
 class Course extends React.Component {
     render() {
-        return <span>{this.props.course}</span>
+        return <span className="p-2">{this.props.course}</span>
     }
 }
 
 class Subrequirement extends React.Component {
     render() {
-        const completed = this.props.data.Needs;
-        const courses = Object.entries(completed ? this.props.data.CompletedCourses : this.props.data.SelectFrom);
+        const id = "requirement_" + this.props.id;
+        const headerId = "req_header_" + this.props.id;
+        const completed = !this.props.data.hasOwnProperty("Needs");
+        const courses = completed ? this.props.data.CoursesTaken : this.props.data.SelectFrom;
+        const expanded = this.props.data.subrequirement.length < 1;
+        const collapseClasses = expanded ? [] : ["collapse"];
+        const headerBg = completed ? "bg-success" : "bg-danger";
 
-        return <div className="subrequirement">
-            <h3>{ this.props.title }</h3>
-            <div className="collapse card">
-                <div className="card-body">
-                    <p><strong>
+        return <div className="subrequirement my-2">
+            <ThemedCard>
+                <div className={["card-header", headerBg].join(" ")} id={headerId}>
+                    <h2 className="mb-0">
                         {
-                            completed ? "You have completed this requirement, satisfied by the following courses" :
-                                `You need ${this.props.data.Needs} credits of the following courses to fulfill this requirement`
+                            expanded ? "" : <button className="btn btn-link text-light" type="button" data-toggle="collapse" data-target={"#" + id} aria-expanded={expanded ? "true" : "false"} aria-controls={id}>
+                                { this.props.data.subrequirement }
+                            </button>
                         }
-                    </strong></p>
-                    <p>
-                        {courses.map(([key, course]) => <Course key={key} course={course} />)}
-                    </p>
+                    </h2>
                 </div>
-            </div>
+                <div id={id} className={collapseClasses} aria-labelledby={headerId}>
+                    <div className="card-body">
+                        <p><strong>
+                            {
+                                completed ? "You have completed this requirement, satisfied by the following courses" :
+                                    `You need ${this.props.data.Needs} of the following courses to fulfill this requirement`
+                            }
+                        </strong></p>
+                        { this.showCourses(courses) }
+                    </div>
+                </div>
+            </ThemedCard>
         </div>
+    }
+
+    showCourses(courses) {
+        if (courses === null || courses === undefined) return "Could not find courses";
+        return <p>
+            {Object.entries(courses).map(([key, course]) => <Course key={key} course={course} />)}
+        </p>
     }
 }
 
-class Requirement extends React.Component {
+class RequirementComponent extends React.Component {
     render() {
-        return <div className="requirement">
-            <h2>{ this.props.title }</h2>
-            <div className="collapse card">
-                <div className="card-body">
-                    { this.props.subrequirements.map((data, i) => <Subrequirement key={i} data={data}  />) }
+        const id = "requirement_" + this.props.id;
+        const headerId = "req_header_" + this.props.id;
+        const textColor = this.props.theme.dark ? "text-light" : "text-dark";
+        return <div className="requirement my-2">
+            <ThemedCard>
+                <div className="card-header" id={headerId}>
+                    <h2 className="mb-0">
+                        <button className={"btn btn-link " + textColor} type="button" data-toggle="collapse" data-target={"#" + id} aria-expanded="true" aria-controls={id}>
+                            { this.props.title }
+                        </button>
+                    </h2>
                 </div>
-            </div>
+                <div id={id} className="collapse show" aria-labelledby={headerId}>
+                    <div className="card-body">
+                        { Object.entries(this.props.subrequirements).map(([key, data]) => <Subrequirement key={key} id={`${this.props.id}_${key}`} data={data}  />) }
+                    </div>
+                </div>
+            </ThemedCard>
         </div>
     }
 }
+const Requirement = connect(({ theme }) => ({theme}), null)(RequirementComponent);
 
 class Requirements extends React.Component {
     constructor(props) {
@@ -62,11 +96,22 @@ class Requirements extends React.Component {
         }
     }
 
+    expandAll() {
+        window.$(".requirement .collapse").collapse('show');
+    }
+
+    collapseSubreqs() {
+        window.$(".subrequirement .collapse").collapse('hide');
+    }
+
+    collapseAll() {
+        window.$(".requirement .collapse").collapse('hide');
+    }
+
     render() {
         if (this.state.loading && this.props.requirements.loaded) this.setState({ loading: false });
 
         return <div>
-            <h1>Requirements</h1>
             { this.props.requirements.error !== null ? this.error() : (this.state.loading ? this.loading() : this.loaded()) }
         </div>
     }
@@ -79,22 +124,38 @@ class Requirements extends React.Component {
     }
 
     loaded() {
+        const bg = this.props.theme.dark ? "bg-secondary" : "bg-gray";
         return <>
-            <h2>These are the requirements you need to fulfill to graduate</h2>
-            <div id="requirements">
-                { this.props.requirements.map(({Title: title, subrequirements}, i) => <Requirement key={i} title={title} subrequirements={subrequirements} />) }
+            <Jumbotron className={bg}>
+                <h1>Requirements</h1>
+                <p>
+                    The following are all of the requirements you need to fulfill in order to graduate.
+                    Each requirement is divided up as subrequirements, although some only have one "subrequirement".
+                    When the subrequirement's header is <span className="text-danger">red</span> you have not completed the
+                    subrequirement, and when it is <span className="text-success">green</span> you have completed it.
+                    To view more info about a requirement or subrequirement, click it's title.
+                </p>
+            </Jumbotron>
+            <div id="requirements" className="container-fluid mt-2">
+                <ButtonGroup size="sm">
+                    <ThemedButton onClick={this.collapseSubreqs}>Collapse Subrequirements</ThemedButton>
+                    <ThemedButton onClick={this.collapseAll}>Collapse All</ThemedButton>
+                    <ThemedButton onClick={this.expandAll}>Expand All</ThemedButton>
+                </ButtonGroup>
+
+                { this.props.requirements.requirements.map(({Title, subrequirements}, i) => <Requirement key={i} id={i} title={Title} subrequirements={subrequirements} />) }
             </div>
         </>
     }
 
     error() {
-        return <Alert variant="danger">
+        return <div className="container-fluid mt-2"><Alert variant="danger">
             <Alert.Heading>Oh no!</Alert.Heading>
             <p>
                 Failed to fetch your requirements list!
             </p>
-        </Alert>
+        </Alert></div>
     }
 }
 
-export default connect(({ requirements, user }) => ({ requirements, user }), { fetchRequirements })(Requirements);
+export default connect(({ requirements, user, theme }) => ({ requirements, user, theme }), { fetchRequirements })(Requirements);
