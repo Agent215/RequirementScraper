88e02490784995fd4ecb342072f921637035f552
Rathanank Onn <tuk86465@temple.edu>
2020-04-15 18:56:30 -0400

    added database function to return jsonify array of [totalCred, registerCred]

---
+++ backend/scraping/calcTotalCredsTaken.py
@@ -0,0 +1,23 @@
+from urllib.request import urlopen
+#get the beautifulSoup module and name as soup
+from bs4 import BeautifulSoup as soup
+from selenium import webdriver
+from flask import jsonify
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+
+#sauce = urllib.request.urlopen('https://prd-dars.temple.edu/selfservice/audit/read.html?id=JobQueueRun!!!!ISEhIWludFNlcU5vPTM2OTg0NjI=').read()
+#soup = bs.BeautifulSoup(sauce, 'lxml')
+#print(soup)
+#print(soup.find_all('p'))
+#print(soup.title)
+
+#START ALGORITHM
+
+#call method
+
+print("hello mac")
