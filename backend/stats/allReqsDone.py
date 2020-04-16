9785276564586f30422ab346080f7baf8121fb24
Abraham Schultz <Abraham.Schultz@gmail.com>
2020-04-15 22:00:26 -0400

    added more stats

--- backend/stats/allReqsDone.py
+++ backend/stats/allReqsDone.py
@@ -6,16 +6,24 @@ import json
 
 
 def allReqsDone(dataIn):
-
     returnData = {}
     data = None
     reqCount = 0
     reqsDone = 0
+    reqsIP = 0
     data = json.loads(dataIn)
     for req in data:
         if req["Status"] == "COMPLETE":
             reqsDone += 1
+        if req["Status"] == "IN PROGRESS":
+            reqsIP += 1
         reqCount += 1
-    percentage = reqsDone/reqCount
-    returnData["percentOfReqsDone"] = percentage
+    percentageDone = float(reqsDone/reqCount)
+    percentageIP = float(reqsIP/reqCount)
+    print("percentage done " ,percentageDone)
+    print("percentage of sub reqs  done " ,percentageIP)
+    returnData["requirementsDone"] = reqsDone
+    returnData["percentOfReqsDone"] = round(percentageDone,3)
+    returnData["requirementsIP"] = reqsIP
+    returnData["percentOfReqsIP"] = round(percentageIP,3)
     return returnData
