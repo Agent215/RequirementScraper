4c268dd07ec9862b1288f6f95b2170ae71a7cf21
Rathanank Onn <tuk86465@temple.edu>
2020-04-18 23:58:20 -0400

    fixed function name and added test for returning GPA

--- backend/testing/statTests.py
+++ backend/testing/statTests.py
@@ -23,22 +23,34 @@ def allReqsDone_count():
     return msg
     
 #testing values for Rathanank to see if method returns correct values to credits taken and regiestered
-def return94and15credits():
+def return94and25credits():
     user = 95 #tuk86465
     totalAndRegisterCredits = {} #dictionary to hold test data
     totalAndRegisterCredits = getTotalCredits(95) #call db function and assign values to dictionary
-    msg = "return94and15credits Passed"
+    msg = "return94and25credits Passed"
 
-    assert totalAndRegisterCredits["totalCredits"] == 96, "return94and15credits Failed"
-    assert totalAndRegisterCredits["registeredCredits"] == 25 , "return94and15credits Failed"
+    assert totalAndRegisterCredits["totalCredits"] == 96, "return94and25credits Failed"
+    assert totalAndRegisterCredits["registeredCredits"] == 25 , "return94and25credits Failed"
     
     return msg
 
+#testing that the function calls, gets and returns the correct GPA from Temple University
+def returnCorrectGPA():
+    user = 95 #hard coded, user ID can be found from database
+    myGPA = readGPA(95) #set value from function call, function call returns a GPA of type double
+    
+    msg = "returnCorrectGPA Passed" #message to print if test passed
+    
+    assert myGPA == 3.25, "returnCorrectGPA failed" #REMARK: MY current GPA for tuk86465 is 3.25
+                                                    #REMARK 2: assert will fail and print failed message if !=
+    return msg #prints only if assert is true
+    
 def runAllTests():
     msg = []
     msg.append(allReqsDone_count())
     msg.append(AllReqsDone_length())
-    msg.append(return94and15credits())
+    msg.append(return94and25credits())
+    msg.append(returnCorrectGPA())
     return jsonify(msg)
 
 
